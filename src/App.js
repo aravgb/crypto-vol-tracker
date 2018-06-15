0f882be45c7b50a22c07178ce7b56a392e8c33de
Jonathan Otto <jonathan.otto@gmail.com>
2018-06-15 15:07:19 -0400

    Simple table of tickers + vol

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import tickers from "./tickers.json";
 
 class App extends Component {
+  renderTicker(t, i) {
+    return (
+      <tr key={i}>
+        <td>{t.name}</td>
+        <td>{t.quotes.USD.volume_24h}</td>
+      </tr>
+    );
+  }
+
+  renderTickers() {
+    const rows = Object.values(tickers.data).map((t, i) =>
+      this.renderTicker(t, i)
+    );
+
+    return (
+      <table>
+        <thead>
+          <td>Ticker</td>
+          <td>Volume</td>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Crypto Vol Ranker</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderTickers()}
       </div>
     );
   }
